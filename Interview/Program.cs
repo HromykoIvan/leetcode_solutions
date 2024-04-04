5af99d02344e5f3cebc13b94cb117c418d19ae24
igromyko <gromyko.ivan.work@gmail.com>
2024-04-04 17:30:54 +0200

    added interview project

---
+++ Interview/Program.cs
@@ -0,0 +1,15 @@
+﻿//Factorial, recursion
+var num = 4;
+var result = CalculateFactorial(num);
+Console.WriteLine($"The factorial of the number {num} is {result}.");
+
+
+int CalculateFactorial(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+
+    return num * CalculateFactorial(num - 1);
+}
