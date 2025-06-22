fbfb2d1ac75614a7f450b40427350d4b9c5375e0
igromyko <gromyko.ivan.work@gmail.com>
2025-06-22 16:18:56 +0200

    added TestEmail project

---
+++ TestEmails/Program.cs
@@ -0,0 +1,7 @@
+var email = "ivan.gromyko@idt.net";
+Console.Write(ValidateEmails(email));
+
+static bool ValidateEmails(string emailAddress)
+{
+    return emailAddress?.Split(',').All(address => EmailValidation.EmailValidator.Validate(address.Trim(), allowInternational: true)) ?? false;
+}
